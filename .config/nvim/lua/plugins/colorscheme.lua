bcad1f23f4c939396bbb50a5579e7f9e34c4cbc9
pigeon <pigeon@quietpigeon.com>
2025-05-11 22:35:58 +0900

    refactor: nvim plugins

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -8,12 +8,4 @@ return {
     name = "catppuccin",
     priority = 1000,
   },
-
-  {
-    "saecki/crates.nvim",
-    tag = "stable",
-    config = function()
-      require("crates").setup()
-    end,
-  },
 }
