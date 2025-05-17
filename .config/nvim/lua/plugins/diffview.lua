f0fbc6c81452f5da26a3037cdec8b7c219841752
pigeon <91387291+QuietPigeon2001@users.noreply.github.com>
2025-05-17 10:27:52 +0900

    add diffview

---
+++ .config/nvim/lua/plugins/diffview.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "sindrets/diffview.nvim",
+    keys = {
+      { "<C-d>", "<cmd>DiffviewOpen<cr>", desc = "Open Diffview" },
+      { "<C-S-d>", "<cmd>DiffviewClose<cr>", desc = "Close Diffview" },
+    },
+  },
+}
